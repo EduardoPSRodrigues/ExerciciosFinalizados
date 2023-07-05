b40a75de770d6575aad6cc5f2f855d60d981c0a5
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-07-05 17:02:35 -0300

    Finalização Exercício 4 - Tabuada

---
+++ Módulo 1 - Semana 5/exercicio4/app.js
@@ -0,0 +1,30 @@
+console.log("Módulo 1 - Semana 5 - Exercício 4");
+
+// --------------------------------------------------------------------------
+// Tabuada chamando uma Função
+// --------------------------------------------------------------------------
+
+const numero = prompt("Informe um número");
+
+console.log("Tabuada chamando uma Função");
+calculaTabuada(numero);
+
+function calculaTabuada(numero) {
+  for (let i = 0; i <= 10; i++) {
+    console.log(`${numero} * ${i} = ${numero * i}`);
+  }
+}
+
+// --------------------------------------------------------------------------
+// Tabuada com Arrow Function
+// --------------------------------------------------------------------------
+
+const calculaTabuada1 = (numero2) => {
+  for (let i = 0; i <= 10; i++) {
+    console.log(`${numero2} * ${i} = ${numero2 * i}`);
+  }
+};
+
+console.log("");
+console.log("Tabuada com Arrow Function");
+calculaTabuada1(numero);
