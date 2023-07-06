979622d7d028960e6506ecebeea7603f1ffe6903
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-07-06 17:48:19 -0300

    Finalização Exercício 8 - PA

--- Módulo 1 - Semana 5/exercicio8/app.js
+++ Módulo 1 - Semana 5/exercicio8/app.js
@@ -1,5 +1,15 @@
 console.log("Módulo 1 - Semana 5 - Exercício 8");
 
-/* --------------------------------------------------------------------------
+const valorInicial = Number(prompt("Valor inicial:"));
+const raiz = Number(prompt("Raiz da PA:"));
 
---------------------------------------------------------------------------*/
+function calculaPA(valorInicial, raiz) {
+  const pa = [valorInicial];
+
+  for (let i = 0; i < 9; i++) {
+    pa.push(pa[i] + raiz);
+  }
+  console.log(pa);
+}
+
+calculaPA(valorInicial, raiz);
