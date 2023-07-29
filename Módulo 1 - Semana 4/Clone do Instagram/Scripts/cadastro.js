d60fef9915c23a4db585fa511416ac4d961a1182
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-07-29 20:53:09 -0300

    Alteração do HTML e JS da tela cadastro
    Adicionando a função fetch para pegar e armazenar em uma variável os dados digitados no formulário

--- Módulo 1 - Semana 4/Clone do Instagram/Scripts/cadastro.js
+++ Módulo 1 - Semana 4/Clone do Instagram/Scripts/cadastro.js
@@ -1,74 +1,104 @@
-var regexTelefone = /^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/;
-var regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+import { regexEmail, regexTelefone } from "../Utilitários/regex.js";
 
-function resetErroFormulario() {
-/* Colocar um reset no input error (vinculado as bordas) */
-document.getElementById("campo-nomeCompleto").classList.remove("input-error")
-document.getElementById("campo-email").classList.remove("input-error")
-document.getElementById("campo-telefone").classList.remove("input-error")
-document.getElementById("campo-nomeUsuario").classList.remove("input-error")
-document.getElementById("campo-senha").classList.remove("input-error")
+// Pegar todas as variáveis do HTML
+  const formularioCadastro = document.getElementById("formulario-Cadastro");
+  const nomeCompleto = document.getElementById("campo-nomeCompleto");
+  const email = document.getElementById("campo-email");
+  const telefone = document.getElementById("campo-telefone");
+  const nomeUsuario = document.getElementById("campo-nomeUsuario");
+  const senha = document.getElementById("campo-senha");
+  const erroNome = document.getElementById("erro-nome");
+  const erroEmail = document.getElementById("erro-email");
+  const erroTelefone = document.getElementById("erro-telefone");
+  const erroNomeUsuario = document.getElementById("erro-nomeUsuario");
+  const erroSenha = document.getElementById("erro-senha");
+  const loginButton = document.getElementById("login-button");
 
-/* Colocar um reset no erro-nome (vinculado as mensagens de erro) */
-document.getElementById("erro-nome").hidden = true;
-document.getElementById("erro-email").hidden = true;
-document.getElementById("erro-telefone").hidden = true;
-document.getElementById("erro-nomeUsuario").hidden = true;
-document.getElementById("erro-senha").hidden = true;
+formularioCadastro.addEventListener("submit", fazerCadastro);
+
+function fazerCadastro(evento) {
+  evento.preventDefault();
+
+  // Função para limpar os erros do formulário
+  resetErroFormulario();
+
+  if (nomeCompleto.value === "") {
+    nomeCompleto.classList.add("input-error");
+    nomeCompleto.focus();
+    erroNome.hidden = false;
+    erroNome.innerText = "O nome é obrigatório";
+  } else if (regexEmail.test(email.value) === false) {
+  /* Começo com o nome escondido e caso caia nesse if o campo irá aparecer */
+    email.classList.add("input-error");
+    email.focus();
+    erroEmail.hidden = false;
+    erroEmail.innerText = "Digite um e-mail válido";
+  } else if (regexTelefone.test(telefone.value) === false) {
+    telefone.classList.add("input-error");
+    telefone.focus();
+    erroTelefone.hidden = false;
+    erroTelefone.innerText = "Digite um telefone válido";
+  } else if (nomeUsuario.value === "") {
+    nomeUsuario.classList.add("input-error");
+    nomeUsuario.focus();
+    erroNomeUsuario.hidden = false;
+    erroNomeUsuario.innerText = "O nome do usuário é obrigatório";
+  } else if (senha.value.length < 8) {
+  /* LENGHT se a constiável senha tiver menos que 9 caracteres, aparece um erro */
+    senha.classList.add("input-error");
+    senha.focus();
+    erroSenha.hidden = false;
+    erroSenha.innerText = "A senha precisa ter no mínimo 8 caracteres";
+  } else {
+    loginButton.disabled = true;
+    loginButton.style.opacity = 0.5;
+    loginButton.innerText = "Logando...";
+
+    //Cadastrar os dados
+    fetch('http://localhost:3333/usuarios', {
+      method: 'POST',
+      body: JSON.stringify ({
+        nomeCompleto: nomeCompleto.value, 
+        email: email.value,
+        telefone: telefone.value,
+        nomeUsuario: nomeUsuario.value,
+        senha: senha.value
+      }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+  }
 }
 
-function fazerCadastro() {
-    
-    // Pegar todas as variáveis do HTML
-    var nomeCompleto = document.getElementById("campo-nomeCompleto").value;
-    var email = document.getElementById("campo-email").value;
-    var telefone = document.getElementById("campo-telefone").value;
-    var nomeUsuario = document.getElementById("campo-nomeUsuario").value;
-    var senha = document.getElementById("campo-senha").value;
-  
-    // Função para limpar os erros do formulário
-    resetErroFormulario()
-  
-    if (nomeCompleto === "") {
-      document.getElementById("campo-nomeCompleto").classList.add("input-error");
-      document.getElementById("campo-nomeCompleto").focus();
-      document.getElementById("erro-nome").hidden = false;
-      document.getElementById("erro-nome").innerText = "O nome é obrigatório"
-    } 
-    /* Começo com o nome escondido e caso caia nesse if o campo irá aparecer */
-    else if (regexEmail.test(email) === false) {
-        document.getElementById("campo-email").classList.add("input-error");
-        document.getElementById("campo-email").focus(); 
-        document.getElementById("erro-email").hidden = false;
-        document.getElementById("erro-email").innerText = "Digite um e-mail válido"
-    }
-    else if (regexTelefone.test(telefone) === false) {
-        document.getElementById("campo-telefone").classList.add("input-error");
-        document.getElementById("campo-telefone").focus(); 
-        document.getElementById("erro-telefone").hidden = false;
-        document.getElementById("erro-telefone").innerText = "Digite um telefone válido"
-    }
-    else if (nomeUsuario === "") {
-        document.getElementById("campo-nomeUsuario").classList.add("input-error");
-        document.getElementById("campo-nomeUsuario").focus(); 
-        document.getElementById("erro-nomeUsuario").hidden = false;
-        document.getElementById("erro-nomeUsuario").innerText = "O nome do usuário é obrigatório"
+function resetErroFormulario() {
+  /* Colocar um reset no input error (vinculado as bordas) */
+  nomeCompleto.classList.remove("input-error");
+  email.classList.remove("input-error");
+  telefone.classList.remove("input-error");
+  nomeUsuario.classList.remove("input-error");
+  senha.classList.remove("input-error");
 
-    }
-    /* LENGHT se a variável senha tiver menos que 9 caracteres, aparece um erro */
-    else if (senha.length < 9) { 
-      document.getElementById("campo-senha").classList.add("input-error");
-      document.getElementById("campo-senha").focus();
-      document.getElementById("erro-senha").hidden = false;
-      document.getElementById("erro-senha").innerText = "A senha precisa ter no mínimo 8 caracteres"
-    } else{
-    document.getElementById("login-button").disabled = true
-    document.getElementById("login-button").style.opacity = 0.5
-    document.getElementById("login-button").innerText = "Logando..."
-  
-    /* window.location.href=""; - Pedir para o Java enviar para outra página, como se 
-    fosse a função "a" do HTML  */
-    window.location.href="https://www.instagram.com/accounts/password/reset/";
-  
-    }
-  }
+  /* Colocar um reset no erro-nome (vinculado as mensagens de erro) */
+  erroNome.hidden = true;
+  erroEmail.hidden = true;
+  erroTelefone.hidden = true;
+  erroNomeUsuario.hidden = true;
+  erroSenha.hidden = true;
+}
+
+/* Informações do projeto:
+* window.location.href=""; - Pedir para o Java enviar para outra página, como se fosse a função "a" do HTML
+* Em relação ao fecth, existem algumas requisições como:
+  GET: lista várias informações de um recurso como: produto, usuários, post. O retorno é em objeto ou um array de objeto.
+  Post: cadastrar um novo recurso
+  Delete: deletar um produto específico
+  Put: atualizar todos os campos de um recurso
+  Patch: atualizar uma coisa especifica  
+
+
+
+
+
+*/
