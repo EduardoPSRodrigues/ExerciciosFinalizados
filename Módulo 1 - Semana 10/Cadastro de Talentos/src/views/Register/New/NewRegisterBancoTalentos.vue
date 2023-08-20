9146bc5cec17e0cf23e3967d27bea42a1e613cad
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-20 19:47:21 -0300

    Finalização do Script da tela de Cadastro

--- Módulo 1 - Semana 10/Cadastro de Talentos/src/views/Register/New/NewRegisterBancoTalentos.vue
+++ Módulo 1 - Semana 10/Cadastro de Talentos/src/views/Register/New/NewRegisterBancoTalentos.vue
@@ -4,15 +4,19 @@
 
         <label for="name">Nome Completo:</label>
         <input type="text" id="name" v-model="name">
+        {{ this.errors.name }}
 
         <label for="email">Email:</label>
         <input type="email" id="email" v-model="email">
+        {{ this.errors.email }}
 
         <label for="dataNascimento">Data de Nascimento:</label>
         <input type="date" id="dataNascimento" v-model="dataNascimento">
+        {{ this.errors.dataNascimento }}
 
         <label for="whatsapp">WhatsApp:</label>
         <input type="phone" id="whatsapp" v-model="whatsapp">
+        {{ this.errors.whatsapp }}
 
         <label for="areaInteresse">Área de Interesse:</label>
         <select id="areaInteresse" v-model="areaInteresse">
@@ -21,6 +25,7 @@
             <option value="Backend">Backend</option>
             <option value="Full Stack">Full Stack</option>
         </select>
+        {{ this.errors.areaInteresse }}
 
         <label for="nivelProfissional">Nível profissional:</label>
         <select id="nivelProfissional" v-model="nivelProfissional">
@@ -29,6 +34,7 @@
             <option value="Pleno">Pleno</option>
             <option value="Senior">Senior</option>
         </select>
+        {{ this.errors.nivelProfissional }}
 
         <p>Selecione suas habilidades:</p>
 
@@ -45,9 +51,11 @@
             <label><input type="checkbox" value="Laravel" v-model="habilidades">Laravel</label>
             <label><input type="checkbox" value="Java" v-model="habilidades">Java</label>
         </div>
+        {{ this.errors.habilidades }}
 
         <label for="CartaApresentacao">Carta de Apresentação:</label>
         <textarea id="CartaApresentacao" cols="30" rows="10" v-model="CartaApresentacao"></textarea>
+        {{ this.errors.CartaApresentacao }}
 
         <button type="submit">Cadastrar</button>
     </form>
@@ -56,6 +64,7 @@
 <script>
 
 import * as yup from 'yup'
+import { captureErrorYup } from "../../../utils/captureErrorYup";
 
 export default {
     data() {
@@ -68,6 +77,8 @@ export default {
             nivelProfissional: '',
             habilidades: [],
             CartaApresentacao: '',
+
+            errors: {},
         }
     },
     methods: {
@@ -93,19 +104,43 @@ export default {
                     areaInteresse: this.areaInteresse,
                     nivelProfissional: this.nivelProfissional,
                     CartaApresentacao: this.CartaApresentacao,
+                }, { abortEarly: false })
+
+                axios({
+                    url: 'http://localhost:3000/api/talent',
+                    method: 'POST',
+                    data: {
+                        name: this.name,
+                        email: this.email,
+                        dataNascimento: this.dataNascimento,
+                        whatsapp: this.whatsapp,
+                        areaInteresse: this.areaInteresse,
+                        nivelProfissional: this.nivelProfissional,
+                        habilidades: this.habilidades,
+                        CartaApresentacao: this.CartaApresentacao
+                    }
                 })
+                    .then(() => {
+                        alert('Cadastrado com sucesso.')
+                    })
+                    .catch(() => {
+                        alert('Houve um erro ao cadastrar.')
+                    })
             } catch (error) {
-                alert('erro no formulário')
+                if (error instanceof yup.ValidationError) {
+                    //Um erro gerado pelo YUP.
+                    this.errors = captureErrorYup(error)
+                    console.log(captureErrorYup(error));
+                }
             }
         },
         watch: {
-            areaInteresse(newValue, oldValue) {
+            areaInteresse: function (newValue, oldValue) {
                 if (newValue !== oldValue) {
                     this.habilidades = []
                 }
-            },
-        },
-
+            }
+        }
     }
 }
 </script>
@@ -129,10 +164,4 @@ export default {
      </label>
      </div> 
 * wacth é um escutado, ou seja, estou monitorando a minha variável habiildades, caso ela seja alterada, o seu valor 
-  automaticamente assume zero.
-    
-    
-    
-    
-    
-    -->
+  automaticamente assume zero. -->
