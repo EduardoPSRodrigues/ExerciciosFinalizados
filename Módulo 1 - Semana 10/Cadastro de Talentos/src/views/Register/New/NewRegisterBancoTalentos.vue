29f50e6151a231e1b66b27c45bbfbaabc42fe646
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-20 13:29:51 -0300

    Construção da tela de cadastro

--- Módulo 1 - Semana 10/Cadastro de Talentos/src/views/Register/New/NewRegisterBancoTalentos.vue
+++ Módulo 1 - Semana 10/Cadastro de Talentos/src/views/Register/New/NewRegisterBancoTalentos.vue
@@ -1,15 +1,105 @@
 <template>
-    <div>
-        TELA CADASTRO
+    <div class="form-new">
+        <h2>TELA CADASTRO</h2>
+
+        <label for="name">Nome Completo:</label>
+        <input type="text" id="name" v-model="name">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="email">
+
+        <label for="dataNascimento">Data de Nascimento:</label>
+        <input type="date" id="dataNascimento" v-model="dataNascimento">
+
+        <label for="whatsapp">WhatsApp:</label>
+        <input type="phone" id="whatsapp" v-model="whatsapp">
+
+        <label for="areaInteresse">Área de Interesse:</label>
+        <select id="areaInteresse" v-model="areaInteresse">
+            <option value="">Selecione um item</option>
+            <option value="Frontend">Frontend</option>
+            <option value="Backend">Backend</option>
+            <option value="Full Stack">Full Stack</option>
+        </select>
+
+        <label for="nivelProfissional">Nível profissional:</label>
+        <select id="nivelProfissional" v-model="nivelProfissional">
+            <option value="">Selecione um item</option>
+            <option value="Junior">Junior</option>
+            <option value="Pleno">Pleno</option>
+            <option value="Senior">Senior</option>
+        </select>
+
+        <p>Selecione suas habilidades:</p>
+
+        <div v-if="areaInteresse === 'Frontend' || areaInteresse === 'Full Stack'">
+            <label><input type="checkbox" value="HTML" v-model="habilidades">HTML</label>
+            <label><input type="checkbox" value="CSS" v-model="habilidades">CSS</label>
+            <label><input type="checkbox" value="Java Script" v-model="habilidades">Java Script</label>
+            <label><input type="checkbox" value="Vue" v-model="habilidades">Vue</label>
+        </div>
+
+        <div v-if="areaInteresse === 'Backend' || areaInteresse === 'Full Stack'">
+            <label><input type="checkbox" value="Node" v-model="habilidades">Node</label>
+            <label><input type="checkbox" value="PHP" v-model="habilidades">PHP</label>
+            <label><input type="checkbox" value="Laravel" v-model="habilidades">Laravel</label>
+            <label><input type="checkbox" value="Java" v-model="habilidades">Java</label>
+        </div>
+
+        <label for="CartaApresentacao">Carta de Apresentação:</label>
+        <textarea id="CartaApresentacao" cols="30" rows="10" v-model="CartaApresentacao"></textarea>
+
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            name: '',
+            email: '',
+            dataNascimento: '',
+            whatsapp: '',
+            areaInteresse: '',
+            nivelProfissional: '',
+            habilidades: [],
+            CartaApresentacao: '',
+        }
+    },
+    watch: {
+        areaInteresse(newValue, oldValue){
+            if (newValue !== oldValue){
+                this.habilidades = []
+            }
+        },
+    },
+
 }
 </script>
 
-<style>
-    
+<style scoped>
+.form-new {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* background-color: red; */
+}
 </style>
+
+<!-- Informações do projeto.
+    
+* Adicionar vários checkbox usando o v-for 
+     <div v-if="areaInteresse === 'Backend' || areaInteresse === 'Full Stack'">
+     <label v-for="(skill, index) in ['Node', 'PHP', 'Laravel', 'Java']" :key="index">
+     <input type="checkbox" v-model="habilidades" :value="skill">{{ skill }}
+     </label>
+     </div> 
+* wacth é um escutado, ou seja, estou monitorando a minha variável habiildades, caso ela seja alterada, o seu valor 
+  automaticamente assume zero.
+    
+    
+    
+    
+    
+    -->
