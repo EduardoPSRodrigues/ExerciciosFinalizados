cd1d13bbd1357332a5b87ea391117037c6d07161
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-20 10:46:44 -0300

    Finalização da configuração de roteamento
    Importar o router e adicionar .use(router) no createApp.
    Por fim, adicionar a tag <router-view> </router-view> no App.vue

--- Módulo 1 - Semana 10/Cadastro de Talentos/src/main.js
+++ Módulo 1 - Semana 10/Cadastro de Talentos/src/main.js
@@ -2,5 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routes/routes.js'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.mount('#app')
+
+/* Importar o router e adicionar .use(router) no createApp. 
+   Por fim, adicionar a tag <router-view> </router-view> no App.vue */
