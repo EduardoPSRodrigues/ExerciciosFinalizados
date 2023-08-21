5cee261aa1123580a8f93d1006bc802377ee4252
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-21 11:25:41 -0300

    Finalização da tela Sobre

--- Módulo 1 - Semana 10/Cadastro de Talentos/src/views/About/AboutBancoTalentos.vue
+++ Módulo 1 - Semana 10/Cadastro de Talentos/src/views/About/AboutBancoTalentos.vue
@@ -1,15 +1,84 @@
 <template>
-    <div>
-        TELA SOBRE
+    <div class="container-texto">
+        <h1>Sobre Nossa Empresa de Banco de Talentos</h1>
+
+        <p>Bem-vindo à Banco de Talentos, onde a inovação encontra o talento e as oportunidades se tornam realidade. Estamos
+            dedicados a construir um ecossistema que une profissionais excepcionais com as empresas que buscam alcançar
+            novos
+            patamares de sucesso. Aqui, acreditamos que o talento certo no lugar certo pode impulsionar o crescimento e a
+            excelência.</p>
+
+        <h3>Nossa Missão</h3>
+
+        <p>Nossa missão é simples, mas fundamental: facilitar a conexão perfeita entre talentos brilhantes e as organizações
+            mais
+            promissoras. Queremos ser o elo que torna possível o encontro entre pessoas talentosas e empresas inovadoras.
+            Acreditamos que, juntos, podemos criar um futuro mais dinâmico e próspero.</p>
+
+        <h3>A Experiência dos Nossos Talentos</h3>
+
+        <p> Para os profissionais, somos o caminho para alcançar o próximo nível de suas carreiras. Nossa plataforma oferece
+            oportunidades estimulantes em uma variedade de setores e funções, permitindo que você explore e desenvolva suas
+            habilidades e paixões. Trabalhamos para capacitá-lo a alcançar suas ambições profissionais e pessoais.</p>
+
+        <h3>O Valor para as Empresas</h3>
+
+        <p>Para as empresas, somos uma fonte confiável de talento de alta qualidade. Nossos serviços de recrutamento e
+            seleção são
+            projetados para fornecer a você os candidatos que melhor atendam às suas necessidades e cultura empresarial.
+            Juntos,
+            ajudaremos sua organização a crescer, inovar e se destacar no mercado.
+        </p>
+        <h3>Nossos Princípios</h3>
+
+        <p>Em cada interação e serviço que oferecemos, aderimos aos mais altos padrões éticos e profissionais. Valorizamos a
+            transparência, a integridade e a confiabilidade em todos os aspectos de nosso trabalho. Nosso compromisso com a
+            igualdade de oportunidades nos permite construir um banco de talentos verdadeiramente diversificado e inclusivo.
+        </p>
+
+        <h3>Inovação Contínua</h3>
+
+        <p>A evolução é uma constante em nosso setor, e estamos sempre à frente, explorando novas tecnologias e práticas
+            para
+            atender às crescentes demandas do mercado. Estamos empenhados em oferecer as melhores soluções de recrutamento e
+            seleção, adaptadas ao cenário em constante mudança dos negócios.
+        </p>
+        <h3>Junte-se a Nós</h3>
+
+        <p>Convidamos você a explorar nosso banco de talentos e descobrir como podemos ajudar a impulsionar sua carreira ou
+            empresa. Seja você um talento em busca de novas oportunidades ou uma organização em busca de pessoas
+            excepcionais,
+            estamos aqui para apoiá-lo em sua jornada. Juntos, podemos criar um futuro de sucesso e crescimento.</p>
+
+        <h3>Obrigado por escolher Banco de Talentos. Estamos ansiosos para trabalhar com você e fazer
+            grandes coisas juntos!</h3>
+
+        <iframe title="google maps"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14699.929567488405!2d-43.20068169218751!3d-22.914020299999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997f5972a8ee71%3A0x223996712d6daf96!2sWinner%20RH%20RJ!5e0!3m2!1spt-BR!2sbr!4v1692626829655!5m2!1spt-BR!2sbr"
+            width="800" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
 <style>
-    
+h1,
+h3 {
+    font-weight: bold;
+}
+
+.container-texto {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+iframe {
+    margin: 0 auto;
+}
 </style>
