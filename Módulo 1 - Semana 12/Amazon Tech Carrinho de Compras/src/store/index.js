1871b47cb333199cc23a76034ebd2347042d065d
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-08-30 07:28:10 -0300

    Adição de comentários do projeto

--- Módulo 1 - Semana 12/Amazon Tech Carrinho de Compras/src/store/index.js
+++ Módulo 1 - Semana 12/Amazon Tech Carrinho de Compras/src/store/index.js
@@ -13,18 +13,24 @@ const store = createStore({
         },
         adicionarProdutoAoCarrinho(state, produtoRecebido) {
 
+            //Estou pegando o valor atual do state do  e procurando o id dele dentro de produtosCarrinho seja igual ao
+            //id que estou recebendo como parametro
             const produtoNoCarrinho = state.produtosCarrinho.find(produto => produto.id === produtoRecebido.id)
-
+            
+            //Se ele encontrar o id, ele ira retornar todas as informações da variável
             if (produtoNoCarrinho) {
-
+                //Se o produto estiver no carrinho, então tenho que percorrer a função e adicionar mais um item
                 state.produtosCarrinho = state.produtosCarrinho.map(item => {
                     if (item.id === produtoRecebido.id) {
                         item.quantidade = item.quantidade + 1
                     }
+                    //Retorna o array atualizado
                     return item
                 })
 
             } else {
+                //Se ele não tiver sido selecionado para ir no carrinho, ele vai manter todas as informações e adicionar
+                //a variável quantidade: 1 - Apenas para ter essa variável
                 state.produtosCarrinho = [
                     ...state.produtosCarrinho,
                     {
